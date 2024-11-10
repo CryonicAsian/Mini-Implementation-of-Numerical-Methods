7e8309d101dbfe797e2b53b301dd390535b01955
CryonicAsian <soumilg12@gmail.com>
2024-11-10 13:08:30 -0500

    1.3
    Finished defining all the functions

--- MiniFunctionTools.cpp
+++ MiniFunctionTools.cpp
@@ -14,3 +14,21 @@ double MiniFunctionTools::BackwardDifference(double x, double h){
 double MiniFunctionTools::CenteredDifference(double x, double h){
     return(func(x + h) - func(x - h)) / 2 * h;
 }
+
+double MiniFunctionTools::LeftHandRectangular(double a, double b, int N){
+    double h = (b - a) / N;
+    double sum = 0.0;
+    for (int i = 0; i < N; i++) {
+        sum += func(a + i * h);
+    }
+    return h * sum;
+}
+
+double MiniFunctionTools::TrapezoidalRule(double a, double b, int N){
+    double h = (b - a) / N;
+    double sum = 0.5 * (func(a) + func(b));
+    for(int i = 1; i < N; i++){
+        sum += func(a + i * h);
+    }
+    return h * sum;
+}
